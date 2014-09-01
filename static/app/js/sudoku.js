57b2bed5975637ae284d09342f5b8f79183f8220
Keith Williams <kswillia@andrew.cmu.edu>
2014-08-31 22:39:59 -0400

    setup require.js structure; testing with Sudoku class

---
+++ static/app/js/sudoku.js
@@ -0,0 +1,15 @@
+define(
+  ['lib/jquery'],
+  function(){
+    function Sudoku() {
+      this.name = 'Sudoku';
+      this.init();
+    }
+
+    Sudoku.prototype.init = function() {
+      $('body').append('<p>' + this.name + '</p>');
+    }
+
+    return new Sudoku();
+  }
+);
